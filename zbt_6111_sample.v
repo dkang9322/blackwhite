d2621e73380a5a68560b67a56e7986c8b92535d1
dkang9322 <wkang@mit.edu>
2013-11-14 19:16:10 -0500

    vram_display module timing and pixel definition changed to accomodate for 18 bit output. not sure of the timing

--- zbt_6111_sample.v
+++ zbt_6111_sample.v
@@ -681,7 +681,7 @@ module vram_display(reset,clk,hcount,vcount,vr_pixel,
    input reset, clk;
    input [10:0] hcount;
    input [9:0] 	vcount;
-   output [7:0] vr_pixel;
+   output [17:0] vr_pixel;
    output [18:0] vram_addr;
    input [35:0]  vram_read_data;
 
@@ -691,23 +691,22 @@ module vram_display(reset,clk,hcount,vcount,vr_pixel,
       
    wire [18:0] 	 vram_addr = {1'b0, vcount_f, hcount_f[9:2]};
 
-   wire [1:0] 	 hc4 = hcount[1:0];
-   reg [7:0] 	 vr_pixel;
+   // hc4 handles the timing, we need to check for 2
+   wire 	 hc4 = hcount[0];
+   reg [17:0] 	 vr_pixel;
    reg [35:0] 	 vr_data_latched;
    reg [35:0] 	 last_vr_data;
 
    always @(posedge clk)
-     last_vr_data <= (hc4==2'd3) ? vr_data_latched : last_vr_data;
+     last_vr_data <= (hc4==1'd1) ? vr_data_latched : last_vr_data;
 
    always @(posedge clk)
-     vr_data_latched <= (hc4==2'd1) ? vram_read_data : vr_data_latched;
+     vr_data_latched <= (hc4==1'd0) ? vram_read_data : vr_data_latched;
 
    always @(*)		// each 36-bit word from RAM is decoded to 4 bytes
      case (hc4)
-       2'd3: vr_pixel = last_vr_data[7:0];
-       2'd2: vr_pixel = last_vr_data[7+8:0+8];
-       2'd1: vr_pixel = last_vr_data[7+16:0+16];
-       2'd0: vr_pixel = last_vr_data[7+24:0+24];
+       1'd1: vr_pixel = last_vr_data[17:0];
+       1'd0: vr_pixel = last_vr_data[35:0];
      endcase
 
 endmodule // vram_display
